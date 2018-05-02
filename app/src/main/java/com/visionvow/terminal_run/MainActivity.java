989abaeb29d78bb9fd89509c51dd25975fa72c7e
Jason Li <visionvow@gmail.com>
2018-05-02 18:39:24 -0400

    Removed status bar/action bar

--- app/src/main/java/com/visionvow/terminal_run/MainActivity.java
+++ app/src/main/java/com/visionvow/terminal_run/MainActivity.java
@@ -1,5 +1,6 @@
 package com.visionvow.terminal_run;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +10,7 @@ import android.widget.ImageView;
 
 import com.visionvow.terminal_run.listeners.ChangeScreenListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         View decorView = getWindow().getDecorView();
         // Hide the status bar.
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
         decorView.setSystemUiVisibility(uiOptions);
 
         setContentView(R.layout.activity_main);
